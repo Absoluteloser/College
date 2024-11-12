875e56c4cabc7f701b4bccca6be35fdaf0d29108
Absoluteloser <motkulwarbalaji805@gmail.com>
2024-11-12 11:42:16 +0530

    buffer,references,memory layout

--- SPOS/src/PageReplacement/FIFO.java
+++ SPOS/src/PageReplacement/FIFO.java
@@ -1,16 +1,27 @@
-package PageReplacement;
+import PageReplacement;
+import java.util.*;
 import java.io.*;
 public class FIFO{
-    public static void main(String[]args) throws IOException
-    {
+    public static void main(String[]args) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int frames,pointer=0,hit=0,fault=0,ref_len;
+        //first variable initialization
+        int frames;
+        int ref_len;
+        int hits=0;
+        int faults=0;
         int buffer[];
-        int reference[];
-        int mem_layout[][];
+        int references[];
+        int memorylayout[][];
         System.out.println("enter the number of frames");
-        frames=Integer.ParseInt(br.readLine());
-        System.out.println("enter the length of reference string");
-        ref_len=Integer.parseInt(br.readLine());
+        frames=Integer.parseInt(br.readline());
+        System.out.println("enter the length of refernce string");
+        ref_len=Integer.parseInt(br.readline());
+        buffer=new int[frames];
+        references=new int[ref_len];
+        memorylayout=new int[ref_len][frames];
+        for(int i=0;i<frames;++i){
+            buffer[i]=-1;
+        }
+        
     }
 }
