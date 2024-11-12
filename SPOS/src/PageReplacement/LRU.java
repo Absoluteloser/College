9575c1487eee1a558d139fffd752de8f18b3bdbb
Absoluteloser <motkulwarbalaji805@gmail.com>
2024-11-12 12:52:23 +0530

    LRU completed using stack

--- SPOS/src/PageReplacement/LRU.java
+++ SPOS/src/PageReplacement/LRU.java
@@ -8,7 +8,7 @@ public class LRU{
         int faults=0;
         int ref_len;
         BufferedReader br=new BUfferedReader(new InputStreamReader(System.in));
-        Boolean isFull=false;
+        Stack<Integer>frameStack=new Stack<>();
         int buffer[];
         int references[];
         int memorylayout[][];
@@ -19,5 +19,39 @@ public class LRU{
         buffer=new int[frames];
         references=new int[ref_len];
         memorylayout=new int[ref_len][frames];
+        System.out.println("Please enter the reference string");
+        for(int i=0;i<ref_len;++i){
+            referenceString[i]=Integer.parseInt(br.readline());
+        }
+        //LRU algorithm implementation using stack
+        for(int i=0;i<ref_len;++i){
+            int currentPage=referenceString[i];
+            if(frameStack.contains(currentPage)){
+                //found
+                frameStack.remove(currentPage);
+                frameStack.push(currentPage);
+                hits++;
+            }
+            //not found
+            if(frameStack.size()==numberOfFrames){
+                //added this dont know why ???????? will see later
+                frameStack.remove(0);
+            }
+            frameStack.push(currentPage);
+            faults++;
+        }
+        for(int j=0;j<frames;++j){
+            memorylayout[i][j]=(j<frameStack.size())?frameStack.get(j):-1;
+        }
+        System.out.println("\nMemory Layout");
+        for(int i=0;i<frames;++i){
+            for(int j=0;j<ref_len;++j){
+                System.out.println("%3d",memorylayout[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("\nThe number of hits"+hits);
+        System.out.println("\nThe number of misses"+faults);
+        System.out.println("hit ratio"+((float)hits/ref_len));
     }
 }
