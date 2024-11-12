2a46e1ec620f01db05692ffa013d5b383ba66108
Absoluteloser <motkulwarbalaji805@gmail.com>
2024-11-12 12:09:34 +0530

    completed code of FIFO

--- SPOS/src/PageReplacement/FIFO.java
+++ SPOS/src/PageReplacement/FIFO.java
@@ -22,6 +22,41 @@ public class FIFO{
         for(int i=0;i<frames;++i){
             buffer[i]=-1;
         }
-        
+        System.out.println("enter the reference string");
+        for(int i=0;i<ref_len;++i){
+            references[i]=Integer.parseInt(br.readLine());
+        }
+        System.out.println("============================================");
+        //traverse through the references array
+        for(int i=0;i<ref_len;++i){
+            int search=-1;
+            for(int j=0;j<frames;++j){
+                if(buffer[j]==references[i]){
+                    search=j;
+                    hits++;
+                    break;
+                }
+            }
+            if(search==-1){
+                buffer[pointer]=references[i];
+                fault++;
+                pointer++;
+                if(pointer==frames){
+                    pointer=0;
+                }
+            }
+            for(int j=0;j<frames;++j){
+                memorylayout[j][i]=buffer[j];
+            }
+        }
+        for(int i=0;i<frames;++i){
+            for(int j=0;j<ref_len;++j){
+                System.out.println("%3d",memorylayout[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("the number of hits"+hits);
+        System.out.println("the ratio:"+(float)((float)hits/ref_len));
+        System.out.println("the number of Faults"+fault);
     }
 }
